2154af189486f6ecfb856a3f7c4eb1530b95e89e
Piedrah <luispiedrahita1131209@correo.itm.edu.co>
2025-08-28 09:12:56 -0500

    Cambios 28/08/2025 9:12

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+public class ConjuntoReci
+{
+    public decimal id;
+    public String? NombreConjunto;
+}
+
+public class Torres
+{
+    public decimal id;
+    public String? NombreTorre;
+    public List<ConjuntoReci>? Conjunto;
+}
+
+public class Habitaciones
+{
+    public decimal id;
+    public String? Nombre;
+    public Torres? Torre;
+}
+
+public class Tipos
+{
+    public decimal id;
+    public String? NombreTipo;
+    public List<Habitaciones>? Habitacion;
+}
